dc8d969bdd5a2389b4182a382c2f6c0f6b4204be
Umut <omid.haqi@outlook.com>
2024-09-09 15:55:28 +0330

    delete some unnecessary file

--- src/api/validations/api.go
+++ src/api/validations/api.go
@@ -1,23 +1,27 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/omidhaqi/clean-web-api/api/routers"
+	"github.com/omidhaqi/clean-web-api/config"
 )
 
 func InitServer() {
+
+	cfg := config.GetConfig()
 
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
 
 	v1 := r.Group("/api/v1/")
 	{
-		
-			health:= v1.Group("/health")
-			routers.Health(health)
-		
+
+		health := v1.Group("/health")
+		routers.Health(health)
 
 	}
-	r.Run(":5005")
+	r.Run(fmt.Sprintf(":%s", cfg.Server.InternalPort))
 
 }
---
+++ src/config/config.go
@@ -0,0 +1,144 @@
+package config
+
+import (
+	"errors"
+	"log"
+	"os"
+	"time"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig
+	Postgres PostgresConfig
+	Redis    RedisConfig
+	Password PasswordConfig
+	Cors     CorsConfig
+	Logger   LoggerConfig
+	Otp      OtpConfig
+	JWT      JWTConfig
+}
+
+type ServerConfig struct {
+	InternalPort    string
+	ExternalPort    string
+	RunMode string
+}
+
+type LoggerConfig struct {
+	FilePath string
+	Encoding string
+	Level    string
+	Logger   string
+}
+
+type PostgresConfig struct {
+	Host            string
+	Port            string
+	User            string
+	Password        string
+	DbName          string
+	SSLMode         string
+	MaxIdleConns    int
+	MaxOpenConns    int
+	ConnMaxLifetime time.Duration
+}
+
+type RedisConfig struct {
+	Host               string
+	Port               string
+	Password           string
+	Db                 string
+	DialTimeout        time.Duration
+	ReadTimeout        time.Duration
+	WriteTimeout       time.Duration
+	IdleCheckFrequency time.Duration
+	PoolSize           int
+	PoolTimeout        time.Duration
+}
+
+type PasswordConfig struct {
+	IncludeChars     bool
+	IncludeDigits    bool
+	MinLength        int
+	MaxLength        int
+	IncludeUppercase bool
+	IncludeLowercase bool
+}
+
+type CorsConfig struct {
+	AllowOrigins string
+}
+
+type OtpConfig struct {
+	ExpireTime time.Duration
+	Digits     int
+	Limiter    time.Duration
+}
+
+type JWTConfig struct {
+	AccessTokenExpireDuration  time.Duration
+	RefreshTokenExpireDuration time.Duration
+	Secret                     string
+	RefreshSecret              string
+}
+
+func GetConfig() *Config {
+	cfgPath := getConfigPath(os.Getenv("APP_ENV"))
+	v, err := LoadConfig(cfgPath, "yml")
+	if err != nil {
+		log.Fatalf("Error in load config %v", err)
+	}
+
+	cfg, err := ParseConfig(v)
+	envPort := os.Getenv("PORT")
+	if envPort != ""{
+		cfg.Server.ExternalPort = envPort
+		log.Printf("Set external port from environment -> %s", cfg.Server.ExternalPort)
+	}else{
+		cfg.Server.ExternalPort = cfg.Server.InternalPort
+		log.Printf("Set external port from environment -> %s", cfg.Server.ExternalPort)
+	}
+	if err != nil {
+		log.Fatalf("Error in parse config %v", err)
+	}
+
+	return cfg
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var cfg Config
+	err := v.Unmarshal(&cfg)
+	if err != nil {
+		log.Printf("Unable to parse config: %v", err)
+		return nil, err
+	}
+	return &cfg, nil
+}
+func LoadConfig(filename string, fileType string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigType(fileType)
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Printf("Unable to read config: %v", err)
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+		return nil, err
+	}
+	return v, nil
+}
+
+func getConfigPath(env string) string {
+	if env == "docker" {
+		return "/app/config/config-docker"
+	} else if env == "production" {
+		return "/config/config-production"
+	} else {
+		return "../config/config-development"
+	}
+}
