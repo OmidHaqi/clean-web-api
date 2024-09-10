f07d07d47badb5b36dc692626704214a20385c27
Umut <omid.haqi@outlook.com>
2024-09-10 22:27:37 +0330

    create some helper

---
+++ src/api/helper/base_response.go
@@ -0,0 +1,45 @@
+package helper
+
+
+import validation "github.com/omidhaqi/clean-web-api/api/validations"
+
+
+type BaseHttpResponse struct {
+	Result           any                           `json:"result"`
+	Success          bool                          `json:"success"`
+	ResultCode       ResultCode                    `json:"resultCode"`
+	ValidationErrors *[]validation.ValidationError `json:"validationErrors"`
+	Error            any                           `json:"error"`
+}
+
+func GenerateBaseResponse(result any, success bool, resultCode ResultCode) *BaseHttpResponse {
+	return &BaseHttpResponse{Result: result,
+		Success:    success,
+		ResultCode: resultCode,
+	}
+}
+
+func GenerateBaseResponseWithError(result any, success bool, resultCode ResultCode, err error) *BaseHttpResponse {
+	return &BaseHttpResponse{Result: result,
+		Success:    success,
+		ResultCode: resultCode,
+		Error:      err.Error(),
+	}
+
+}
+
+func GenerateBaseResponseWithAnyError(result any, success bool, resultCode ResultCode, err any) *BaseHttpResponse {
+	return &BaseHttpResponse{Result: result,
+		Success:    success,
+		ResultCode: resultCode,
+		Error:      err,
+	}
+}
+
+func GenerateBaseResponseWithValidationError(result any, success bool, resultCode ResultCode, err error) *BaseHttpResponse {
+	return &BaseHttpResponse{Result: result,
+		Success:          success,
+		ResultCode:       resultCode,
+		ValidationErrors: validation.GetValidationErrors(err),
+	}
+}
