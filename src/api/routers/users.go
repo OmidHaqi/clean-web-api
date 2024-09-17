22c69bb9c441ff607d758b66c7b96816dad745f1
Umut <omid.haqi@outlook.com>
2024-09-17 19:45:25 +0330

    add Limiter for otp

--- src/api/routers/users.go
+++ src/api/routers/users.go
@@ -3,12 +3,13 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/omidhaqi/clean-web-api/api/handlers"
+	"github.com/omidhaqi/clean-web-api/api/middlewares"
 	"github.com/omidhaqi/clean-web-api/config"
 )
 
 func User(routers *gin.RouterGroup,cfg *config.Config)  {
 	var h = handlers.NewUserHandler(cfg)
 	
-	routers.POST("/send-otp", h.SendOtp)
+	routers.POST("/send-otp", middlewares.OtpLimiter(cfg),h.SendOtp)
 	
 }
