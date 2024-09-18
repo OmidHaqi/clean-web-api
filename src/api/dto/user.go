254a04b85e70cf773f46b1c85a7a68216a490c20
Umut <omid.haqi@outlook.com>
2024-09-18 11:59:52 +0330

    create json web token

--- src/api/dto/user.go
+++ src/api/dto/user.go
@@ -3,3 +3,11 @@ package dto
 type GetOtpRequest struct {
 	MobileNumber string `json:"mobileNumber" binding:"required,mobile,min=11,max=11"`
 }
+
+type TokenDetails struct {
+	AccessToken            string `json:"accessToken"`
+	RefreshToken           string `json:"refreshToken"`
+	AccessTokenExpireTime  int    `json:"accessTokenExpireTime"`
+	RefreshTokenExpireTime int    `json:"refreshTokenExpireTime"`
+}
+
--- src/pkg/service_errors/error_code.go
+++ src/pkg/service_errors/error_code.go
@@ -1,7 +1,24 @@
 package service_errors
 
 const (
-	OtpExistsError = "otpExistsError"
-	OtpUsedError   = "otpUsedError"
-	OtpNotValidError   = "otpNotValidError"
+	// Token
+	UnExpectedError = "Expected error"
+	ClaimsNotFound  = "Claims not found"
+	TokenRequired   = "token required"
+	TokenExpired    = "token expired"
+	TokenInvalid    = "token invalid"
+
+	// OTP
+	OtpExistsError   = "Otp exists"
+	OtpUsedError     = "Otp used"
+	OtpNotValidError = "Otp invalid"
+
+	// User
+	EmailExists               = "Email exists"
+	UsernameExists            = "Username exists"
+	PermissionDenied          = "Permission denied"
+	UsernameOrPasswordInvalid = "username or password invalid"
+
+	// DB
+	RecordNotFound = "record not found"
 )
