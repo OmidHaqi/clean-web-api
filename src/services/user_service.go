776474e070a8fb7bc29243583b69a824803200c0
Umut <omid.haqi@outlook.com>
2024-09-18 12:00:08 +0330

    some function for register

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -4,13 +4,15 @@ import (
 	"github.com/omidhaqi/clean-web-api/api/dto"
 	"github.com/omidhaqi/clean-web-api/common"
 	"github.com/omidhaqi/clean-web-api/config"
+	"github.com/omidhaqi/clean-web-api/constants"
+	"github.com/omidhaqi/clean-web-api/data/models"
 	"github.com/omidhaqi/clean-web-api/infra/persistence/database"
 	"github.com/omidhaqi/clean-web-api/pkg/logging"
 	"gorm.io/gorm"
 )
 
 type UserService struct {
-	logger     *logging.Logger
+	logger     logging.Logger
 	cfg        *config.Config
 	otpService *OtpService
 	database   *gorm.DB
@@ -22,7 +24,7 @@ func NewUserService(cfg *config.Config) *UserService {
 	return &UserService{
 		cfg: cfg,
 		database: database,
-		logger: &logger,
+		logger: logger,
 		otpService: NewOtpService(cfg),
 	}
 }
@@ -36,4 +38,54 @@ func (s *UserService) SendOtp(req *dto.GetOtpRequest) error {
 	}
 	return nil 
 	
+}
+
+func (s *UserService) existsByEmail(email string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("email = ?", email).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByUsername(username string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("username = ?", username).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByMobileNumber(mobileNumber string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("mobile_number = ?", mobileNumber).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) getDefaultRole() (roleId int, err error) {
+
+	if err = s.database.Model(&models.Role{}).
+		Select("id").
+		Where("name = ?", constants.DefaultRoleName).
+		First(&roleId).Error; err != nil {
+		return 0, err
+	}
+	return roleId, nil
 }
