f3de7b4cfc4ac1131041f1ba8a71f171706a57d6
Umut <omid.haqi@outlook.com>
2024-10-17 20:25:38 +0330

    update doc

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -1511,7 +1511,7 @@ const docTemplate = `{
                 "error": {},
                 "result": {},
                 "resultCode": {
-                    "type": "integer"
+                    "$ref": "#/definitions/github_com_omidhaqi_clean-web-api_api_helper.ResultCode"
                 },
                 "success": {
                     "type": "boolean"
@@ -1523,6 +1523,31 @@ const docTemplate = `{
                     }
                 }
             }
+        },
+        "github_com_omidhaqi_clean-web-api_api_helper.ResultCode": {
+            "type": "integer",
+            "enum": [
+                0,
+                40001,
+                40101,
+                40301,
+                40401,
+                42901,
+                42902,
+                50001,
+                50002
+            ],
+            "x-enum-varnames": [
+                "Success",
+                "ValidationError",
+                "AuthError",
+                "ForbiddenError",
+                "NotFoundError",
+                "LimiterError",
+                "OtpLimiterError",
+                "CustomRecovery",
+                "InternalError"
+            ]
         },
         "github_com_omidhaqi_clean-web-api_api_validations.ValidationError": {
             "type": "object",
