b02146c074ca098203943bf0a8c6849cf24d98e7
Umut <omid.haqi@outlook.com>
2025-04-16 15:20:35 +0330

    chore: refactor TypeConverter function to return value instead of pointer; add TODO for struct mapping; update ToSnakeCase comment

--- src/common/strings.go
+++ src/common/strings.go
@@ -151,6 +151,7 @@ func HasDigits(s string) bool {
 	return false
 }
 
+// To snake case : CountryId -> country_id
 func ToSnakeCase(str string) string {
 	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
